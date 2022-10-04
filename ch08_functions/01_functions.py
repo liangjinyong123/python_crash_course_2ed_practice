8ce42de564d9751c5484b6aded789cb60e386980
liangjinyong123 <liangjy881@naver.com>
2022-10-04 10:39:07 +0900

    Finish Chapter 8: Functions

--- ch08_functions/01_functions.py
+++ ch08_functions/01_functions.py
@@ -179,3 +179,36 @@ show_completed_models(completed_models)
 
 ## Preventing a Function from Modifying a List
 # print_models(unprinted_designs[:], completed_models)
+
+# Passing an Arbitrary Number of Arguments
+def make_pizza(*toppings):
+	"""Summarize the pizza we are about to make."""
+	print("\nMaking a pizza with the following toppings:")
+	for topping in toppings:
+		print(f"- {topping}")
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+## Mixing Positional and Arbitrary Arguments
+def make_pizza(size, *toppings):
+	"""Summarize the pizza we are about to make."""
+	print(f"\nMake a {size}-inch pizza with the follow toppings:")
+	for topping in toppings:
+		print(f"- {topping}")
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+## Using Arbitrary Keyword Arguments
+def build_profile(first, last, **user_info):
+	"""Build a dictionary containing everything we know about a user."""
+	user_info['first_name'] = first
+	user_info['last_name'] = last
+	return user_info
+
+user_profile = build_profile('albert', 'einstein', 
+		location='princeton',
+		field='physics')
+
+print(user_profile)
