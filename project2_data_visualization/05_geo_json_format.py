57d231714f3824b537258787cad094a5a66af8e0
liangjinyong123 <liangjy881@naver.com>
2022-10-06 11:11:13 +0900

    Finish Chapter 16: Downloading Data

--- project2_data_visualization/05_geo_json_format.py
+++ project2_data_visualization/05_geo_json_format.py
@@ -4,10 +4,12 @@
 from pathlib import Path
 import json
 
+import plotly.express as px
+
 # Read data as a string and convert to a Python object.
-path = Path('eq_data/eq_data_1_day_m1.geojson')
-contents = path.read_text()
-all_eq_data = json.loads(contents)
+# path = Path('eq_data/eq_data_1_day_m1.geojson')
+# contents = path.read_text()
+# all_eq_data = json.loads(contents)
 
 # Create a more readable version of the data file.
 # path = Path('eq_data/readable_eq_data.geojson')
@@ -16,13 +18,97 @@ all_eq_data = json.loads(contents)
 
 ## Making a List of All Earthquakes
 # Examine all earthquakes in the dataset.
-all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+# all_eq_dicts = all_eq_data['features']
+# print(len(all_eq_dicts))
 
 ## Extracting Magnitudes
-mags = []
+# mags = []
+# for eq_dict in all_eq_dicts:
+#     mag = eq_dict['properties']['mag']
+#     mags.append(mag)
+
+# print(mags[:10])
+
+## Extracting Location Data
+# mags, lons, lats = [], [], []
+# for eq_dict in all_eq_dicts:
+#     mag = eq_dict['properties']['mag']
+#     lon = eq_dict['geometry']['coordinates'][0]
+#     lat = eq_dict['geometry']['coordinates'][1]
+#     mags.append(mag)
+#     lons.append(lon)
+#     lats.append(lat)
+
+# print(mags[:10])
+# print(lons[:5])
+# print(lats[:5])
+
+## Building a World Map
+# title = 'Global Earthquakes'
+# fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+# fig.show()
+
+## Representing Magnitudes
+# Read data as a string and convert to a Python object. 
+# path = Path('eq_data/eq_data_30_day_m1.geojson')
+# contents = path.read_text()
+# all_eq_data = json.loads(contents)
+
+# all_eq_dicts = all_eq_data['features']
+
+# mags, lons, lats = [], [], []
+# for eq_dict in all_eq_dicts:
+#     mag = eq_dict['properties']['mag']
+#     lon = eq_dict['geometry']['coordinates'][0]
+#     lat = eq_dict['geometry']['coordinates'][1]
+#     mags.append(mag)
+#     lons.append(lon)
+#     lats.append(lat)
+
+# title = 'Global Earthquakes'
+# fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title)
+# fig.show()
+
+## Customizing Marker Colors
+# fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
+#         color=mags,
+#         color_continuous_scale='Viridis',
+#         labels={'color': 'Magnitude'},
+#         projection='natural earth',
+#     )
+# fig.show()
+
+## Other Color Scales
+# print(px.colors.named_colorscales())
+
+## Adding Hover Text
+path = Path('eq_data/eq_data_30_day_m1.geojson')
+contents = path.read_text()
+all_eq_data = json.loads(contents)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    eq_titles.append(eq_title)
 
-print(mags[:10])
+title = 'Global Earthquakes'
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title)
+fig.show()
+
+# Customizing Marker Colors
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
+        color=mags,
+        color_continuous_scale='Viridis',
+        labels={'color': 'Magnitude'},
+        projection='natural earth',
+        hover_name=eq_titles,
+    )
+fig.show()
